b0424c0a578b5a52e9eff6c88385deb31cbd1a93
Ramazan Ağaslan <agaslanramazan274@gmail.com>
2025-01-19 03:44:39 +0300

    i fixed date function bug

--- src/utils/post.js
+++ src/utils/post.js
@@ -3,6 +3,6 @@
 export function createDateFormat(createdAt){
     let months_tr = ["Ocak","Şubat","Mart","Nisan","Mayıs","Haziran","Eylül","Ekim","Kasım","Aralık"]
     let date = new Date(createdAt);
-    let date_str = date.getDay() + " " + months_tr.at(date.getMonth()) + " " + date.getFullYear();
+    let date_str = date.getDate() + " " + months_tr.at(date.getMonth()) + " " + date.getFullYear();
     return date_str;
 }
